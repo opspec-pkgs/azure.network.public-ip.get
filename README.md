c2b0c23724d2b07e3405db5039cf079112520c49
zach.puckett@nintex.com <zach@dokuforest.com>
2017-07-10 21:04:44 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# problem statement
+gets a public ip from azure network resource
+
+# example usage
+
+> note: in examples, VERSION represents a version of the azure.network.public-ip.get pkg
+
+## install
+
+```shell
+opctl pkg install github.com/opspec-pkgs/azure.network.public-ip.get.get#VERSION
+```
+
+## run
+
+```
+opctl run github.com/opspec-pkgs/azure.network.public-ip.get.get#VERSION
+```
+
+## compose
+
+```yaml
+run:
+  op:
+    pkg: { ref: github.com/opspec-pkgs/azure.network.public-ip.get.get#VERSION }
+    inputs:
+      subscriptionId:
+      loginId:
+      loginSecret:
+      name:
+      resourceGroup:
+      # end optional args
+      loginTenantId:
+      loginType:
+      # end optional args
+    outputs:
+      value:
+```
---
+++ cmd.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+set -e
+
+### begin login
+loginCmd='az login -u "$loginId" -p "$loginSecret"'
+
+# handle opts
+if [ "$loginTenantId" != " " ]; then
+    loginCmd=$(printf "%s --tenant %s" "$loginCmd" "$loginTenantId")
+fi
+
+case "$loginType" in
+    "user")
+        echo "logging in as user"
+        ;;
+    "sp")
+        echo "logging in as service principal"
+        loginCmd=$(printf "%s --service-principal" "$loginCmd")
+        ;;
+esac
+eval "$loginCmd" >/dev/null
+
+echo 'setting default subscription'
+az account set --subscription "$subscriptionId"
+### end login
+
+echo 'getting public ip'
+value=$(az network public-ip show \
+    --name "$name" \
+    --resource-group "$resourceGroup" \
+    --query 'ipAddress' \
+    --output json)
+
+echo "value=$value"
